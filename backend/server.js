21a1187b02172165555c4893de20b6999c03b6c4
james-eo <jameseokonkwo@gmail.com>
2025-02-10 11:47:14 +0100

    Moved database connection to a different file for separation of concern

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,21 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import connectDB from './database/db.js';
 import cors from 'cors';
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
-
 app.get('/', (req, res) => {
   res.send('AdFriend API is running');
 });
 
 app.listen(PORT, () => {
+    connectDB();
   console.log(`Server is running on port ${PORT}`);
 })
